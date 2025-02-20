8ecd8e6a5620b3d2c26ad0bd3136bcb9068b84d1
Eloi Shema <shemaeloi8@gmail.com>
2025-02-20 09:56:57 +0200

    fine tuning tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: "Kanit",
+        montserrat: "Montserrat",
+      },
+      colors: {
+        bgBlack: "rgb(9, 16, 15)",
+        blueGreen: "rgb(0, 255, 241)",
+        myGreen: "rgb(0, 255, 191)",
+        dimGreen: "rgb(18, 255, 196)",
+        goldYellow: "rgb(216, 184, 0)",
+        dimGold: "rgba(255, 200, 10, 0.833)",
+        bgGreen: "rgba(8, 255, 193, 0.15)",
+      },
+      screens: {
+        xs: "320px",
+        sm: "400px",
+        md: "769px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+      height: {
+        "screen-h": "100svh",
+      },
+    },
+  },
+  plugins: [],
+};
